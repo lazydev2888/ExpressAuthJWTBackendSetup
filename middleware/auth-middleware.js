85ff025049e328ca299caef2c62f91fd52ec8997
lazydevcoder <lazydevcoder@gmail.com>
2022-12-29 11:33:15 +0530

    Now UserLogin,UserRegitration, UserChangePAssword, LoggedUserDetails,SendMail for password Reset is working fine

--- middleware/auth-middleware.js
+++ middleware/auth-middleware.js
@@ -1,4 +1,29 @@
-import { Jwt } from "jsonwebtoken";
-import UserModel from "../models/User";
+import jwt from "jsonwebtoken";
+import UserModel from "../models/User.js";
 
-const checkUserAuth = async(req,res)=>
+const checkUserAuth = async(req,res, next )=>{
+    let token 
+    const { authorization } = req.headers
+    if (authorization && authorization.startsWith('Bearer')) {
+        try {
+            token = authorization.split(' ')[1] 
+            // console.log("Token", token)
+            // console.log("Authorization", authorization)
+
+            //  Verify Token
+            const {userID} = jwt.verify(token,process.env.JWT_SECRET_KEY)
+            console.log(userID);
+
+            // GET User from Token
+            req.user = await UserModel.findById(userID).select('-password')
+            next()
+        } catch (error) {
+            console.log(error)
+            res.status(401).send({"status" : "failed", "message" : "Unathorized User"})
+        }
+    }
+    if(!token){
+        res.status(401).send({"status":"failed", "message": "Unathorized User, No Token"})
+    }
+}
+ export default checkUserAuth
